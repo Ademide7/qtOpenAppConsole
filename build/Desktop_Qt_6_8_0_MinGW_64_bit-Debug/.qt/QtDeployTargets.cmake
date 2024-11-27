4d3f2f38f918df3b383e24d3d8ec1112b8b7a81e
Ademide Ibrahim <Ademide.Ibrahim@wemabank.com>
2024-11-27 10:01:45 +0100

    Initial commit of Qt project

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_OpenAppsConsole_FILE C:/Users/LENOVO/Documents/OpenAppsConsole/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/OpenAppsConsole.exe)
+set(__QT_DEPLOY_TARGET_OpenAppsConsole_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_OpenAppsConsole_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
